343ca1894eeb14bbcf2f48b5f5f3cef154f9a12c
Chris Zoellick <czoellick@jewishseniorlife.org>
2022-01-30 23:02:17 -0500

    initial commit

---
+++ js/example.js
@@ -0,0 +1,43 @@
+const items = [];
+
+const listItems = document.querySelector("#list-items");
+
+const updateItemsList = () => {
+  console.log(listItems);
+  if (items.length === 0) {
+    listItems.innerHTML = "You have no items in your list";
+  } else {
+    const itemCount = items.length;
+    listItems.innerHTML = `You have ${
+      items.length
+    } item(s) in your list. <br/>${items.join(", ")}`;
+    console.log(items);
+  }
+};
+
+const handleClick = (e) => {
+  const id = e.target.dataset.id;
+  items.push(id);
+  updateItemsList();
+};
+
+const doTheThing = (arg) => {
+  if (arg.toLowerCase() === "zhu li") {
+    const x = document.querySelector("li#example");
+    if (x) {
+      console.log("trying");
+      x.style.display = "block";
+    }
+  }
+};
+window.onload = () => {
+  const x = document.querySelector("li#example");
+  x.style.display = "none";
+};
+
+const itemButtons = document.querySelectorAll(".item-button");
+
+for (let i = 0; i < itemButtons.length; i++) {
+  const button = itemButtons[i];
+  button.addEventListener("click", handleClick);
+}
---
+++ js/script1.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+  console.log('window loaded')
+  const btn3 = document.querySelector("#btn3");
+  if (btn3 !== null) {
+    btn3.onclick = function () {
+      alert("clicked button 3");
+    };
+  }
+}
+
+console.log('script loaded');
